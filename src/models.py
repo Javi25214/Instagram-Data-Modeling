ca0363ec22ef4efcf0db67336550f1fec2fe6d52
Javi25214 <javitoyrosita@gmail.com>
2023-04-05 08:11:51 +0000

    primer commit

--- src/models.py
+++ src/models.py
@@ -8,26 +8,50 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Follower(Base):
+    __tablename__ = "follower"
+    user_from_id = Column (Integer, ForeignKey("user.id"))
+    user_to_id = Column (Integer, ForeignKey("user.id"))
 
     def to_dict(self):
         return {}
+
+
+class User(Base):
+    __tablename__ = "user"
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+    class Comment(Base):
+        __tablename__ = "comment"
+        id = Columm(Integer, primary_key=True)
+        comment_text = Column(String(250), nullable=False)
+        author_id = Column(Integer, ForeignKey("user.id"))
+
+
+class Media(Base):
+    __tablename__ = "media"
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    type = Column
+    url = Column(String)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+
+    def to_dict(self):
+        return {}
+
+class Post(Base):
+    __tablename__ = "post"
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post = relationship(Post)
+    
 
 ## Draw from SQLAlchemy base
 try:
